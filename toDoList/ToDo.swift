cfbbc998b6860910421f161a4d6fbfe43c047a88
Ilana Rubin <ilanarubin007@gmail.com>
2019-07-12 16:01:42 -0700

    Working Iteration 0

---
+++ toDoList/ToDo.swift
@@ -0,0 +1,13 @@
+//
+//  ToDo.swift
+//  toDoList
+//
+//  Created by Ilana Rubin on 7/12/19.
+//  Copyright © 2019 Ilana Rubin. All rights reserved.
+//
+
+import UIKit
+class ToDo{
+    var name = ""
+    var important = false
+}
